f88618d74c23eeebbfe47bc7fd2dcb5bd126292c
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-01-22 17:35:55 +0100

    Add files via upload

---
+++ java1_html.py
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset = "utf-8">  
+        <p>SALUT</p>
+
+        <link rel="stylesheet" href="java.css" />
+        <script src = "java.js"></script>
+    </head>
+
+    
+    <body>
+        <a href="java_html.py">Grand py 1</a>
+        coucouuuuusalit
+    </body>
+
+
+
+</html>
---
+++ java_html.py
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>  
+        <meta charset = "utf-8">  
+        <title> Site de GrandPY </title>
+
+
+        <img src="image/image1.jpg" border ="6"; alt="logo" style="position:absolute;top:0px;left:0px;"/>
+
+            
+        <h1 class="titre1">Bienvenu sur le site de jb </h1>
+
+        <p class = "phrase-accroche"> Phrase d'accroche, ne met rien de grossier,
+        ne met rien de grossier, ne met rien de grossier !</p>
+              
+        <link rel="stylesheet" href="java.css" />
+        <script src = "java.js"></script>
+        
+    </head>
+
+
+
+
+    
+    <body>
+
+
+        
+        <div class="menu">Menu</div>
+  
+        <ul>
+            <li><a href="java1.html">Grand py 2</a></li>
+            <li></li>
+            <li></li>
+            <li>salut</li>
+        </ul>
+
+
+        <h2> Entre un site  :</h2>
+                                        
+        <form method="post" id = "formulaire">
+            <p>
+                <label for="formulaire"></label>
+                <input type="text" name="formulaire" id="formulaire"
+                placeholder = "ex: openclassrooms" />
+            </p>     
+        </form>
+
+
+
+
+
+
+        
+    
+    </body>
+
+    </br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
+    </br>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+</html>
