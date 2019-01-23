f062e2e21cc4ffb2c28a12f6d770cd6a96e78735
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-01-23 11:19:11 +0100

    Update java2_html.py

--- java2_html.py
+++ java2_html.py
@@ -2,20 +2,42 @@
 <html>
     
     <head>
+    
         <meta charset = "utf-8">
-        <div id="fixe">
-        <a href="https://www.youtube.com/watch?v=e6nwg_0lINc"> lien <a>
-        <a href="https://openclassrooms.com/fr/courses"> lien <a>
+
+
+        <title>Menu</title>
+
+        <h1>MENU</h1>
+        <div class="menuenhaut">
+
+
+            <a href="https://www.youtube.com/watch?v=e6nwg_0lINc"> Dinotopia la vie en dino <a>
+            <a href="https://openclassrooms.com/fr/courses"> ONE <a>
+            <a href="https://openclassrooms.com/fr/courses"> TWO <a>
+            <a href="https://openclassrooms.com/fr/courses"> THREE <a>
+            <a href="https://openclassrooms.com/fr/courses"> FOR <a>
+            <a href="https://openclassrooms.com/fr/courses"> FIVE <a>
       
-</div>
+        </div>
+
+        <link rel="stylesheet" href="java2_css.css" />
     </head>
 
 
 
     <body>
-
-
+        
+        <ul>
+            <li><a href="java_html.html">Grand py 1</a></li>
+            <li><a href="java1_html.html">Grand py 2</a></li>
+        </ul>
+       
     </body>
+
+
+
+
 
 
 
