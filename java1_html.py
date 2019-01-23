6871d83f11e5b6f71a27f8a374850333abf4571f
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-01-23 10:21:42 +0100

    Update java1_html.py

--- java1_html.py
+++ java1_html.py
@@ -1,18 +1,39 @@
+
 <!DOCTYPE html>
 <html>
     
     <head>
         <meta charset = "utf-8">  
-        <p>SALUT</p>
+        <h1>SALUT</h1>
 
-        <link rel="stylesheet" href="java.css" />
+        <MARQUEE scrollamount="4">
+            <IMG src="one.jpg">
+            <IMG src="two.jpg">
+            <IMG src="tree.jpg">
+            <IMG src="for.jpg">
+        </MARQUEE>
+
+
+
+
+
+
+    
+        <link rel="stylesheet" href="java1_css.css" />
         <script src = "java.js"></script>
     </head>
 
     
     <body>
-        <a href="java_html.py">Grand py 1</a>
-        coucouuuuusalit
+       
+        <div class="block">
+            <ul>
+                <li><a href="java_html.html">Grand py 1</a></li>
+                <li>coucouuuuusalit<li>
+                <li><a href="java2_html.html">Page menu</a></li>
+                
+            </ul>
+        </div>
     </body>
 
 
