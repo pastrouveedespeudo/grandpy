998310aa7c1769733a99ef9f34e42b2c0bcdd7d7
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-01-23 10:22:15 +0100

    Add files via upload

---
+++ java2_html.py
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset = "utf-8">
+        <div id="fixe">
+        <a href="https://www.youtube.com/watch?v=e6nwg_0lINc"> lien <a>
+        <a href="https://openclassrooms.com/fr/courses"> lien <a>
+      
+</div>
+    </head>
+
+
+
+    <body>
+
+
+    </body>
+
+
+
+</html>
