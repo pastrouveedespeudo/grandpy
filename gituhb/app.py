3abaff29c393eedd7c6c2d6e4ea138996a5741e4
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-01-28 15:00:50 +0100

    Update app.py

--- gituhb/app.py
+++ gituhb/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from mocks impos Post
 
 app = Flask(__name__)
 
@@ -19,6 +19,21 @@ def contact():
     return render_template("pages/contact_html.html")
 
 
+@app.route('/blog')
+def blog():
+    posts = Post.all()
+    return render_template("posts/posts_html.html", posts=posts)
+
+
+@app.route('/blog/posts/<int:id>')
+def post_show(id):
+
+
+    post = Post.find(id)
+    return render_template("posts/show_html.html", post = post)
+
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("errors/404.html"), 404
@@ -28,3 +43,22 @@ if __name__=="__main__":
     
     app.run(debug=True, port=3000)
     home()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
