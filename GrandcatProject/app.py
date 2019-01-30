0ffc561b8a79e2908108b2b469c67a1439204a35
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-01-30 21:46:40 +0100

    Update app.py

--- GrandcatProject/app.py
+++ GrandcatProject/app.py
@@ -1,23 +1,38 @@
+import os
+import bs4 as bs
+import urllib.request
 from PIL import Image
 from flask import Flask
+from flask import request
 from flask import url_for
 from flask import render_template
-
 from flask_bootstrap import Bootstrap
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 Bootstrap(app)
 
+LISTE = []
 
-@app.route('/')
+
+
+@app.route('/', methods=["GET","POST"])
 def home():
-    return render_template("home.html")
+    yo = {"ok":"ta DAZDZgros pd"}
+    print("salut")
+    if request.method == "POST":
+        message = request.form["input"]
+        LISTE.append(message)
+        return render_template("home.html",message=message)
+    
+
+    else:
+
+        return render_template("home.html")
 
 @app.route('/about')
 def about():
     return render_template("pages/about.html")
-
 
 
 @app.errorhandler(404)
