f646641de23c640ac1da26e3d08b9b7531917580
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-01 16:01:57 +0100

    Update app.py

--- GrandcatProject/app.py
+++ GrandcatProject/app.py
@@ -5,6 +5,7 @@ from PIL import Image
 from flask import Flask
 from flask import request
 from flask import url_for
+from flask import jsonify
 from flask import render_template
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -12,28 +13,70 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 Bootstrap(app)
 
+
 LISTE = []
 
 
-
-@app.route('/', methods=["GET","POST"])
+@app.route('/', methods=["GET", "POST"])
 def home():
-    yo = {"ok":"ta DAZDZgros pd"}
-    print("salut")
-    if request.method == "POST":
-        message = request.form["input"]
-        LISTE.append(message)
-        return render_template("home.html",message=message)
-    
 
-    else:
+    yo = request.args.get("input")
+ 
+    path = "https://www.google.com/maps/search/{}".format(yo)
+    print(yo,"548498464874648")
+    print("COUCOUUUUUUUUUUUUUUUUUUU")
+    print("COUCOUUUUUUUUUUUUUUUUUUU")
+    print("COUCOUUUUUUUUUUUUUUUUUUU")
+    print("COUCOUUUUUUUUUUUUUUUUUUU")
+    return render_template("home.html",path=path)
 
-        return render_template("home.html")
 
 @app.route('/about')
 def about():
-    return render_template("pages/about.html")
 
+    a = request.args.get("a",0,type=int)
+    b = request.args.get("b",0,type=int)
+    resutat=a + b
+    print(resutat)
+    return render_template("pages/about.html",resutat=resutat)
+
+
+@app.route('/yo',methods=['GET','POST']) 
+def yo():
+
+    if request.method == "GET":
+        return render_template("pages/yo.html")
+    else:
+        print("post")
+        print(request.args)
+        return render_template("pages/yo.html")
+        
+
+
+@app.route('/demo')
+def demo():
+    return render_template("pages/demo.html")
+
+
+
+
+
+
+
+
+@app.route('/registration', methods=['GET','POST'])
+def process():
+    yo = request.args.get("input")
+ 
+    if request.method == "POST":
+        message = request.form["input"]
+        LISTE.append(message)
+        
+        return render_template("pages/registration.html",message=LISTE)
+    
+    else:
+
+        return render_template("pages/registration.html")
 
 @app.errorhandler(404)
 def page_not_found(error):
