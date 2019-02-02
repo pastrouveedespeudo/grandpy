4aa58ae36c1fef881b9848fa2d6c1f8592304b66
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-02 22:07:40 +0100

    Update parsage.py

--- GrandcatProject/mock/parsage.py
+++ GrandcatProject/mock/parsage.py
@@ -4,8 +4,17 @@ geocoder = Nominatim()
 adresse = "openclassrooms"
 location = geocoder.geocode(adresse, True, 30)
 print((location.latitude, location.longitude))
+print(location.address)
 
 
-#ok refais les cours ok t'as vu pleins de tuto cool mais y'a certain truk que tu sais pas faire comme ajax
+#(48.8747786, 2.3504885)
+#OpenClassRooms, 7, Cité Paradis, Porte-St-Denis, 10e, Paris, Île-de-France, France métropolitaine, 75010, France
+#faut mettre les coordonnées dans le truk
 
-#jy comprend rien, fais ajax et refais les cours et t'aura juste a mettre lalti et la longi et c bon
+#je sais faut utiliser appi google map mais au pire prend un air ébété e tu dis oui mais ca va plus vite, ou ca revient au meme
+
+#suivis d'un je sais pas ou au pire fais le faut juste faire un truk et jlutilise en affichant la map 
+
+#nik passons au design mtn :D et fais une mini base de donnée
+
+#avec register pour compenser ca 
