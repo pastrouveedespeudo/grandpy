3b50c87a311182104d1580ecc2096b11c48e8b74
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-02 21:44:28 +0100

    Update app.py

--- GrandcatProject/app.py
+++ GrandcatProject/app.py
@@ -1,4 +1,6 @@
 import os
+import subprocess  
+
 import bs4 as bs
 import urllib.request
 from PIL import Image
@@ -11,16 +13,39 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 Bootstrap(app)
 
 
 LISTE = []
 
+    
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+
+
+class Echange(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    dateHeure = db.Column(db.String(80), unique=True)
+    message = db.Column(db.Text(200), unique=True)
+
+
+
+
+
+
 
 @app.route('/', methods=["GET", "POST"])
 def home():
 
-    yo = request.args.get("input")
+    yo = request.form['test']
  
     path = "https://www.google.com/maps/search/{}".format(yo)
     print(yo,"548498464874648")
@@ -34,55 +59,60 @@ def home():
 @app.route('/about')
 def about():
 
-    a = request.args.get("a",0,type=int)
-    b = request.args.get("b",0,type=int)
-    resutat=a + b
-    print(resutat)
-    return render_template("pages/about.html",resutat=resutat)
+    
+    return render_template("pages/about.html")
 
 
-@app.route('/yo',methods=['GET','POST']) 
-def yo():
 
-    if request.method == "GET":
-        return render_template("pages/yo.html")
-    else:
-        print("post")
-        print(request.args)
-        return render_template("pages/yo.html")
+   
         
-
-
 @app.route('/demo')
 def demo():
     return render_template("pages/demo.html")
 
 
-
-
+@app.route("/recup_data",methods=["POST"])
+def recup_data():
+    return jsonify({"a": "dict", "returned": "becomes", "a": "JSON object"}) 
 
 
 
 
 @app.route('/registration', methods=['GET','POST'])
-def process():
+def registration():
+  
+   
     yo = request.args.get("input")
  
-    if request.method == "POST":
-        message = request.form["input"]
-        LISTE.append(message)
-        
-        return render_template("pages/registration.html",message=LISTE)
+
+    print(yo,"                 yoooooooooooooo")
+
+
+    return render_template("pages/registration.html",message=yo)
+   
+
+@app.route('/yo', methods=['GET','POST'])
+def yo():
+    return render_template("pages/yo.html")
+
+
+@app.route('/coucou', methods=['GET','POST'])
+def coucou():
+    yo = request.args.get("test")
+    print(yo)
+    return render_template("pages/coucou.html")
+
     
-    else:
-
-        return render_template("pages/registration.html")
-
 @app.errorhandler(404)
 def page_not_found(error):
+    
     return render_template("errors/404.html"), 404
 
+
+
+
+
 if __name__=="__main__":
-    
+    db.create_all()
     app.run(debug=True, port=3000)
     home()
