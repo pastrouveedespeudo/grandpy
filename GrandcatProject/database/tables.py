96114c91060c25cb976bdb8aeb243b0a0e74f35e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-02-06 16:17:20 +0100

    Update tables.py

--- GrandcatProject/database/tables.py
+++ GrandcatProject/database/tables.py
@@ -1,19 +1,22 @@
 class table:
 
-    def create_table_category(self):
+    def create_table_message(self):
         """This is category table"""
 
         user = "jbaw"
 
         self.cursor.execute("""
-        CREATE TABLE IF NOT EXISTS echange (
+        CREATE TABLE IF NOT EXISTS message (
 
-            id_categorie INT UNSIGNED NOT NULL AUTO_INCREMENT,
-            echange TEXT NOT NULL,
+            id_message INT UNSIGNED NOT NULL AUTO_INCREMENT,
+            message TEXT NOT NULL,
             dateHeure DATETIME NOT NULL,
+            utilisateur VARCHAR(50),
     
-            PRIMARY KEY(id_categorie) )
+            PRIMARY KEY(id_message) )
 
             ENGINE=InnoDB;
 
             """)
+
+        #ajouter utilisateur
