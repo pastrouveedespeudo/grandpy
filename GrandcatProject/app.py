c1886be07bfc1bd42dbf4c0fcfb859b9f5413743
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-03-13 10:17:12 +0100

    Update app.py

--- GrandcatProject/app.py
+++ GrandcatProject/app.py
@@ -1,311 +1,161 @@
-import os
-import subprocess  
-from geopy.geocoders import Nominatim
-import request
-from bs4 import *
-import urllib.request
-from PIL import Image
-from flask import Flask
-from flask import request
-from flask import url_for
-from flask import jsonify
-from geopy.geocoders import Nominatim
-from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import mysql.connector
-import requests
-import wikipediaapi
-import random
-
-app = Flask(__name__)
-
-
-
-LISTE = []
-LISTE_PHRASE = []
-LISTE_WIKI = []
-LOCALISATION_WIKI = []
-
-LISTE_PAYS = [" France", " France métropolitaine", " Paris"]
-
-def searching(parametre):
-    """Here we searching from Python modul(geopy.geocoders)"""
-    """address from the input from html page"""
-
-    geocoder = Nominatim(user_agent="app.py")
-    #parametre is data recup from data()
+<!DOCTYPE html>
+<html>
+    <head>
     
-    location = geocoder.geocode(parametre, True, 30)
-    localisation = location.address
-    localisation = str(localisation)
+        <meta charset="utf-8"/>
+           
+        <title>GrandPY</title>
 
-    #define data from geopy.geocoders into var
-    a = location.address
-    b = location.latitude
-    c = location.longitude
+   
 
-    return a, b, c
+        <link  rel="stylesheet" href="/static/styles/css/home.css" />
 
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script src="/static/styles/js/home2.js" type="text/javascript"></script>
 
-
-
-                 
-@app.route('/')
-def home():
-    """Here we just display home page"""
-
-    title_page = "HOME"
-    return render_template("home.html",title_page=title_page)
-
-
-
-
-@app.route('/wiki', methods=["POST"]) 
-def wiki():
-    b = ""
-    liste = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
-    data_wiki = request.form['data']
-
-    nettoyage = apostrohpe(data_wiki)
-    dernier_mot = parsing_texte(nettoyage)
-    
-    
-    search = searching(dernier_mot)
-
-
-
-    c = 0
-    for i in search[0]:
         
-        if i == ",":
-            c+=1
+        <link rel="shortcut icon" href="/static/img/favicon.ico">
+        
 
-        else:
-            liste[c].append(i)
-            
+
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+
+	<script async defer
+    		src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCt2O8fe5cHDLkjjFk4TQ9Os5Y3vFGmqU8&callback=initMap">
+	</script>
+
+	
+        <div class="container">
+          <div class="row">
+          
+            <div class="col-lg-1 col-xs-12">
+                <a href ="yo"><img src="static/images/image1.jpg" id="logo" alt="logo"/></a>
+            </div>
+
     
-    c1 = 0
+            
+            <div class="col-lg-10 col-xs-12">
+              <h1>HOME</h1>
+              <h2>Hey I'm GrandCat</h2>
+              <h3>Site exclusivement réservé à la recherche d'adresse</h3>
+          </div>
+          
+         </div>
+      </div>
 
 
-    parsing_list = []
-    parsing_list_2 = []
 
-    no_number = ""
-    no_country = ""
 
-    integer = False
+        </head>
+            
+        <body>
+            
 
-    for i in liste:
-        i = "".join(i)
+
+  <div class="essais" id ="essaie"></div>
+
+
+  </form>
+      
+    <div class="container">
+      <div class="row">
+      
+        <div id="image1" class="col-lg-2 col-xs-12">
+          <img id="tourne" src="static/images/tourne/one.jpg"/>
+          
+        </div>
+        
+      
+  
+        <br>
+        <div id="map" class="col-lg-6 col-xs-12">
+          <img src="static/images/yo.jpg" id="payschat"/>
+          <img src="static/images/p.jpg" id="payschat"/>
+        </div>
+
+      
+    </div>
   
 
-        for j in i:
-            try:
-                j = int(j)
-                if type(j) == int:
-
-                    integer = True
-            except:
-                pass
-
-        if integer == True:
-            pass
-        else:
-            parsing_list.append(i)
-            
-        integer = False
-
-
-
-    country = False
-    for i in parsing_list:
-   
-        if i == []:
-            pass
-
-        else:
-            for j in LISTE_PAYS:
-                if j == i:
-                    country = True
-            
-            if country == True:
-                pass
-
-            else:
-                parsing_list_2.append(i)
-                
-        country = False
-
-
-
-    parsing_list_2 = parsing_list_2[1:]
-    for i in parsing_list_2:
-        if i == "":
-            pass
-     
-        else:
-            print(i)
-            wiki_wiki = wikipediaapi.Wikipedia('fr')
-            page_py = wiki_wiki.page('{}'.format(i[1:]))
-            
-            existe = page_py.exists()
-            if existe == True:
-                break
-
-
-    sentence_from_grandpy = ["chochochocolat", "en voici en voila",
-                             "vive les patates", "dites oui pas non, c jb le plus bo ?",
-                             "TU PEUX PAS TESTE SHYVA chai pas qui c"]
-    
-
-    choix = random.choice(sentence_from_grandpy)
-    
-    page = ("<strong>" + str(choix) + ":" +"</strong>" + "<br>" + str(page_py.sections[0:200]) + "...")
-
-
-   
-    if data_wiki:
-        return jsonify({'data':page})
-
   
+    <div class="container">
+      <div class="row">
 
-
-@app.route('/data', methods=["POST"])
-def data():
-    """Here, we just recup data with request form"""
-    """from jquerry function() who define content from input"""
+        <div id="monCadreWiki" class="class="col-lg-6 col-xs-12"></div>
     
-    data = request.form['data']
+        <div id="monCadre", class="class="col-lg-6 col-xs-12"></div>
+        <br>
+        <div class="class="col-lg-4 col-xs-12" id="inp">
+          <form id="form_data">
 
-    nettoyage = apostrohpe(data)
-    dernier_mot = parsing_texte(nettoyage)
+            <input type="text" class="classRecupInfo" id="idRecupInfo" size="80" 
+                placeholder="Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?"/><br>
 
+      
+            <input type="submit" class="classButtonInput" id="idButtonInput" value="Enter"/>
+            <center><img src="/static/images/chat_home.jpg" alt="imagecat" /></center>
 
-     
-    
-    
-    date = datetime.now()
-    date = str(date)
-    
-    LISTE.append("<div style='font-style:italic;'>A {}</div>".format(date))
+         </form>
+         
 
-    LISTE.append("<div><strong>Votre question: </strong></div>")
-    LISTE.append("<div style='font-style:italic'>{}</div>".format(data))
-    
-
-    
-    if data:
-        
-        var = searching(dernier_mot)
-   
-        LISTE.append("<div style='font-style:italic'><strong>Addresse trouvée: </strong></div>")
-        LISTE.append("<div><strong>{}</strong></div>".format(var))
-        LISTE.append("<br>")
-        
-        return jsonify({'data':LISTE})
+         
+       </div>
 
 
-
-    return jsonify({'error':'...'})
-
-
-  
+</div>
+</div>
+      
+      <div id="monCadreAlert"></div>  
     
 
 
-def parsing_texte(data):
-    """Here we'll go to parsing data"""
-    """if user input sentences: Salut GrandPY ! Est-ce que tu connais l'adresse de"""
-    """we juste take the last word from the sentece"""
+      <div id = "heure" class ="heure"></div>
 
-    liste = []
-    liste2 = [[],[],[],[],[],[],[],[],[],[],[],[],[]]
-
-    
-    phrase_accroche = "Salut GrandPY ! Est-ce que tu connais le adresse"
-    a = str(data).find(str(phrase_accroche))
-
-    if a >= 0 :
-        mot = apostrohpe(data)
-        liste.append(mot)
-
-        c=0
-        for i in liste:
-            for j in i :
-                liste2[c].append(j)
-                if j == " ":
-                    c+=1
-
-        c=0
-        for i in liste2:
-            if i == "" or i == [] :
-               pass
-            else:
-                c+=1
-        liste2 = liste2[0:c]
-        dataa = "".join(liste2[-1])
-        LISTE_PHRASE.append(dataa)
-        
-        
-
-        return dataa
-
-    else:
-   
-        return data
-
-
-
-def apostrohpe(data):
-
-   
-    phrase_accroche = "Salut GrandPY ! Est-ce que tu connais l'adresse"
-    
-    a = str(data).find(str(phrase_accroche))
-    
-    if a >= 0:
-        
-        liste = []
-        for i in data:
-            liste.append(i)
       
 
-        mot = []
-        c = 0
-        for i in liste:
-            
-            if liste[c] == "'":
-                liste[c] = "e"
-                mot.append(liste[c])
-                mot.append(" ")  
-            else:
-                mot.append(liste[c])    
-                
-            c+=1
 
-        mot = "".join(mot)
+      <div class="divFavorite" id="divFavorite"></div>
 
-        return mot
+      
 
-    else:
-        return data
-
-
-
+   
 
 
     
-@app.errorhandler(404)
-def page_not_found(error):
-    
-    return render_template("errors/404.html"), 404
 
 
-if __name__== "__main__":
-    
 
-    app.run(debug=True, port=3000)
-    home()
-     
+        </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
